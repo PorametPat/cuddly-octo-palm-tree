f040a9f9e033738443e9f37f0d16574234e1b65b
PorametPat <43396870+PorametPat@users.noreply.github.com>
2025-09-07 13:22:10 +0700

    Updates

---
+++ _app/immutable/chunks/CGMgvc3G.js
@@ -0,0 +1,4 @@
+const l=[{title:"Hello world",summary:"This is my first post!",content:`# Hello world
+ 
+This is my first post!
+... rest of the markdown content`,_meta:{filePath:"hello-world.md",fileName:"hello-world.md",directory:".",extension:"md",path:"hello-world"},slug:"hello-world"}];export{l as a};
---
+++ _app/immutable/entry/start.Hd0BOTjF.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BWg1Jl4T.js";export{o as load_css,r as start};
